d6d08784585ea1da1c513ae0313ed4913fece2c3
John <hello@johnmyrda.com>
2017-06-11 13:17:30 -0700

    Added Power LED pin stuff and FastLED.show() statement

--- src/multi-beacon.cpp
+++ src/multi-beacon.cpp
@@ -10,6 +10,10 @@
 #include "patterns/Usa.h"
 #include <FastLED.h>
 
+#define DEBUG
+
+#define POWER_LED_PIN A3
+
 #define LED_PIN 4
 #define NUM_LEDS_PER_BEACON 24
 #define NUM_BEACONS 6
@@ -22,16 +26,26 @@ CRGB leds[NUM_LEDS];
 PatternController pattern_master = PatternController(leds, NUM_LEDS);
 
 void setup(){
+  #ifdef DEBUG
+    Serial.begin(115200);
+  #endif
   FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalSMD5050);
-  pattern_master.add(new Usa());
+  FastLED.setBrightness(50);
+  pattern_master.add(new Usa(8));
   pattern_master.add(new ColorBars(CRGB::Blue, 4));
-  pattern_master.add(new Gradient());
-  pattern_master.add(new LarsonScanner());
-  pattern_master.add(new Rainbow(8));
+  pattern_master.add(new LarsonScanner(CRGB::Green, CRGB::Orange));
   pattern_master.add(new RandomColors(3));
+  pattern_master.add(new Rainbow(8));
   pattern_master.add(new Sos());
+  pattern_master.add(new Gradient());
+  pattern_master.set_fps(30);
+  pattern_master.pattern_duration = 10000;
+  //Status pin so I know that we made it through Setup
+  pinMode(POWER_LED_PIN, OUTPUT);
+  digitalWrite(POWER_LED_PIN, HIGH);
 }
 
 void loop(){
   pattern_master.show();
+  FastLED.show();
 }
